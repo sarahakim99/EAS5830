982abf358dbafd0358341522d32136726f7b306c
sarahakim99 <63162252+sarahakim99@users.noreply.github.com>
2025-06-26 09:59:09 +0100

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -26,8 +26,8 @@ def connect_with_middleware(contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-    bnb_url = "https://data-seed-prebsc-1-s1.binance.org:8545/"
-    w3 = Web3(HTTPProvider(bnb_url))
+    	bnb_url = "https://data-seed-prebsc-1-s1.binance.org:8545/"
+    	w3 = Web3(HTTPProvider(bnb_url))
 
 	# The second section requires you to inject middleware into your w3 object and
 	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
@@ -35,10 +35,10 @@ def connect_with_middleware(contract_json):
 
 
     # Inject middleware for BNB (POA) chains
-    w3.middleware_onion.inject(ExtraDataToPOAMiddleware(), layer=0)
+    	w3.middleware_onion.inject(ExtraDataToPOAMiddleware(), layer=0)
 
     # Create contract object
-    contract = w3.eth.contract(address=address, abi=abi)
+    	contract = w3.eth.contract(address=address, abi=abi)
 
 	return w3, contract
 

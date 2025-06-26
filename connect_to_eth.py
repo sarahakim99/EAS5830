316c5c7814a1c842af89062ddff82de29745db6f
sarahakim99 <63162252+sarahakim99@users.noreply.github.com>
2025-06-26 10:00:06 +0100

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -11,37 +11,36 @@ infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
 '''
 
 def connect_to_eth():
-	url = "https://mainnet.infura.io/v3/def845368e8e47529180a15c63d275dc"  # FILL THIS IN
-	w3 = Web3(HTTPProvider(url))
-	assert w3.is_connected(), f"Failed to connect to provider at {url}"
-	return w3
+    url = "https://mainnet.infura.io/v3/def845368e8e47529180a15c63d275dc"  # FILL THIS IN
+    w3 = Web3(HTTPProvider(url))
+    assert w3.is_connected(), f"Failed to connect to provider at {url}"
+    return w3
 
 
 def connect_with_middleware(contract_json):
-	with open(contract_json, "r") as f:
-		d = json.load(f)
-		d = d['bsc']
-		address = d['address']
-		abi = d['abi']
+    with open(contract_json, "r") as f:
+        d = json.load(f)
+        d = d['bsc']
+        address = d['address']
+        abi = d['abi']
 
-	# TODO complete this method
-	# The first section will be the same as "connect_to_eth()" but with a BNB url
-    	bnb_url = "https://data-seed-prebsc-1-s1.binance.org:8545/"
-    	w3 = Web3(HTTPProvider(bnb_url))
+    # TODO complete this method
+    # The first section will be the same as "connect_to_eth()" but with a BNB url
+    bnb_url = "https://data-seed-prebsc-1-s1.binance.org:8545/"
+    w3 = Web3(HTTPProvider(bnb_url))
 
-	# The second section requires you to inject middleware into your w3 object and
-	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
-	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
-
+    # The second section requires you to inject middleware into your w3 object and
+    # create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
+    # and https://web3py.readthedocs.io/en/stable/web3.contract.html
 
     # Inject middleware for BNB (POA) chains
-    	w3.middleware_onion.inject(ExtraDataToPOAMiddleware(), layer=0)
+    w3.middleware_onion.inject(ExtraDataToPOAMiddleware(), layer=0)
 
     # Create contract object
-    	contract = w3.eth.contract(address=address, abi=abi)
+    contract = w3.eth.contract(address=address, abi=abi)
 
-	return w3, contract
+    return w3, contract
 
 
 if __name__ == "__main__":
-	connect_to_eth()
+    connect_to_eth()

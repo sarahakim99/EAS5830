a1c90ccea7abd702166a9f981ac8f7cb3af9351f
sarahakim99 <63162252+sarahakim99@users.noreply.github.com>
2025-07-11 13:31:07 +0100

    Update gen_keys.py

--- gen_keys.py
+++ gen_keys.py
@@ -1,6 +1,7 @@
 from web3 import Web3
 from eth_account.messages import encode_defunct
 import eth_account
+from eth_account import Account
 import os
 
 def sign_message(challenge, filename="secret_key.txt"):

